c72389f79b9eb34fcd07d4f4bb46b399ce7d5cb6
epiekarski6666 <epiekarski6666@gmail.com>
2019-01-23 21:37:38 +0100

    BMI

--- src/main/java/BMITest/exercise/BMI.java
+++ src/main/java/BMITest/exercise/BMI.java
@@ -9,6 +9,22 @@ public class BMI {
 
     public String checkBmi(int weight, double growth) {
         String result = "";
+        if (growth == 0) {
+            result = "do not divide by 0";
+        } else if (growth < 0 || weight < 0) {
+            result = "I don't think so";
+        } else {
+            double BMI = weight / (growth * growth);
+            if (BMI <= 18.5) {
+                result = ("get some weight");
+            } else if (BMI >= 25) {
+                result = "you need to grow";
+            } else if (BMI <= 0) {
+                result = "I don't think so";
+            } else {
+                result = "good weight";
+            }
+        }
         return result;
     }
 }
